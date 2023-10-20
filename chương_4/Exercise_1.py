a4be3b7ea05b4a9da7530bd1df2b0b42489dda30
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-20 23:58:45 +0700

    #4 [ Chapter 4 ] update exercise

---
+++ chương_4/Exercise_1.py
@@ -0,0 +1,17 @@
+# MẢNG LÀ GÌ?
+empty_list = [] #Khởi tạo danh sách rỗng
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #Khởi tạo danh sách số nguyên
+names = ["Alice", "Bob", "Phong", "Tài"] #Khởi tạo danh sách chuỗi
+mixed_list = [1, "Alice", True] #Khởi tạo danh sách có các kiểu dữ liệu khác nhau
+print(empty_list)
+print(numbers)
+print(names)
+print(mixed_list)
+
+# Truy xuất từng phân tử bằng index, phân tử đầu tiên có số thứ tự là 0
+names = ["An", "Hòa", "Nam", "Tài", "Hào"]
+print(names[0]) #output: An
+print(names[1]) #output: Hòa
+print(names[2]) #output: Nam
+print(names[3]) #output: Tài
+print(names[4]) #output: Hào
