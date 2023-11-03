7741d5173faa4db3c5b68bda709194f330f907a2
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-11-03 22:26:55 +0700

    #5 [ Chapter_5] update ex_3

---
+++ chương_5/Exercise_3.py
@@ -0,0 +1,21 @@
+# Danh sách Tuple
+my_tuple = ('p','e','r','m','i','t')
+ 
+# Lấy phần tử đầu tiên trong tuple
+print(my_tuple[0])
+# Output: 'p'
+ 
+# Lấy phần tử thứ 6 trong tuple
+print(my_tuple[5])
+# Output: 't'
+ 
+# Mỗi phần tử của tuple là một mảng hoặc 1 tuple khác
+n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+ 
+# Lấy phần tử thứ 4 của phần tử thứ nhất
+print(n_tuple[0][3])
+# Output: 's'
+ 
+# Lấy phần tử thứ 2 của phần tử thứ 2
+print(n_tuple[1][1])
+# Output: 4
