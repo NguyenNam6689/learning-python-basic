9deca69dba3a857c9cc54a67e50df49195d4adb4
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-11-12 22:30:28 +0700

    #5 [ Chapter_5 ] update ex_5

---
+++ chương_5/Exercise_5.py
@@ -0,0 +1,17 @@
+my_tuple = ('p','r','o','g','r','a','m','i','z')
+ 
+# Lấy phần tử thứ 2 đến thứ 4
+print(my_tuple[1:4])
+# Output: ('r', 'o', 'g')
+ 
+# Phần tử đầu tiên đến thứ hai (tức thứ 7 tính từ sau tới)
+print(my_tuple[:-7])
+# Output: ('p', 'r')
+ 
+# Phần tử thứ 8 đến cuối
+print(my_tuple[7:])
+# Output: ('i', 'z')
+ 
+# Lấy toàn bộ phần tử
+print(my_tuple[:])
+# Output: ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
