b8d761316522b2718dceab7e75dcabbe1360ce9c
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-11-12 22:31:21 +0700

    #5 [ Chapter_5 ] update ex_7

---
+++ chương_5/Exercise_7.py
@@ -0,0 +1,11 @@
+dict = {'Ten': 'Hoang', 'Tuoi': 7, 'Ten': 'Nam'};
+print ("dict['Ten']: ", dict['Ten'])
+
+print("================================================")
+
+data1={'Id':100, 'Ten':'Thanh', 'Nghenghiep':'Developer'}
+data2={'Id':101, 'Ten':'Chinh', 'Nghenghiep':'Trainer'}
+print ("Id cua nhan vien dau tien la",data1['Id'])
+print ("Id cua nhan vien thu hai la",data2['Id'])
+print ("Ten cua nhan vien dau tien la:",data1['Ten'])
+print ("Nghe nghiep cua nhan vien thu hai la:",data2['Nghenghiep'])
